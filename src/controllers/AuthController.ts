fb1268beda46eefa8e37f5157b129b787b94bb57
Everth Paredes <epe0413@gmail.com>
2024-08-04 17:37:00 -0600

    Algorithm to create accounts

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,8 +1,16 @@
 import type { Request, Response } from "express"
+import User from "../models/User"
 
 export class AuthController {
 
     static createAccount = async (req: Request, res: Response) => {
-        res.send('desde /api/auth')
+        try {
+            const user = new User(req.body)
+            await user.save()
+
+            res.send('Cuenta creada, revisa tu email para confirmar')
+        } catch (error) {
+            res.send(500).json({error: 'Hubo un error'})
+        }
     }
 }
