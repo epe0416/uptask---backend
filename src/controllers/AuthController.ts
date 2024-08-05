b958a553caf3d61c7ece8166f5eba846f614c053
Everth Paredes <epe0413@gmail.com>
2024-08-04 18:16:28 -0600

    Hashing passwords

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,16 +1,21 @@
 import type { Request, Response } from "express"
 import User from "../models/User"
+import { hashPassword } from "../utils/auth"
 
 export class AuthController {
 
     static createAccount = async (req: Request, res: Response) => {
         try {
+            const {password} = req.body
             const user = new User(req.body)
+            
+            // Hash Password
+            user.password = await hashPassword(password)
             await user.save()
 
             res.send('Cuenta creada, revisa tu email para confirmar')
         } catch (error) {
-            res.send(500).json({error: 'Hubo un error'})
+            res.status(500).json({error: 'Hubo un error'})
         }
     }
 }
