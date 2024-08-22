682c33fe410d02308e68f67d7ceeb7222d9fd407
Everth Paredes <epe0413@gmail.com>
2024-08-22 09:55:30 -0600

    Create an Endpoint and Hook with user information

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -27,13 +27,12 @@ export const authenticate = async (req: Request, res: Response, next: NextFuncti
             const user = await User.findById(decoded.id).select('_id name email')
             if(user) {
                 req.user = user
+                next()
             }else {
                 res.status(500).json({error: 'Token no valido'})
             }
         }
     } catch (error) {
         res.status(500).json({error: 'Token no valido'})
-    }
-
-    next()
+    }    
 }
