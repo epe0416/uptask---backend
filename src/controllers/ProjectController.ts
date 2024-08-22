d60bfbccf45a93bf3177383d7404dd7f5d247d6d
Everth Paredes <epe0413@gmail.com>
2024-08-22 07:34:57 -0600

    Prevent a user who does not have permissions from updating or deleting projects

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -59,6 +59,10 @@ export class ProjectController {
                 const error = new Error('Proyecto no encontrado')
                 return res.status(404).json({error: error.message})
             }
+            if(project.manager.toString() !== req.user.id.toString()) {
+                const error = new Error('Solo el manager puede actualizar el proyecto')
+                return res.status(404).json({error: error.message})
+            }
 
             project.clientName = req.body.clientName
             project.projectName = req.body.projectName
@@ -77,6 +81,10 @@ export class ProjectController {
             const project = await Project.findById(id)
             if(!project) {
                 const error = new Error('Proyecto no encontrado')
+                return res.status(404).json({error: error.message})
+            }
+            if(project.manager.toString() !== req.user.id.toString()) {
+                const error = new Error('Solo el manager puede eliminar el proyecto')
                 return res.status(404).json({error: error.message})
             }
             await project.deleteOne()
