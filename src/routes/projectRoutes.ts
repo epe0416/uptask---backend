7e5c1eb344f887f70314b70269df84d401221ce4
Everth Paredes <epe0413@gmail.com>
2024-08-21 21:29:13 -0600

    Filtering User Projects

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -20,7 +20,7 @@ router.post('/',
     handleInputErrors,
     ProjectController.createProject);
 
-router.get('/', ProjectController.getAllProjects);
+router.get('/', authenticate, ProjectController.getAllProjects);
 
 router.get('/:id',
     param('id').isMongoId().withMessage('ID no válido'),
