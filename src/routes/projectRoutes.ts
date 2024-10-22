e2b7b9ef8a021399aa3235694402aacd93afd35b
Everth Paredes <epe0413@gmail.com>
2024-10-22 10:42:49 -0600

    Creating the path to create Notes

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -7,6 +7,7 @@ import { projectExists } from "../middleware/project";
 import { hasAuthorization, taskBelongsToProject, taskExists } from "../middleware/task";
 import { authenticate } from "../middleware/auth";
 import { TeamMemberController } from "../controllers/TeamController";
+import { NoteController } from "../controllers/NoteController";
 
 const router = Router();
 
@@ -118,4 +119,14 @@ router.delete('/:projectId/team/:userId',
     handleInputErrors,
     TeamMemberController.removeMemberById
 )
+
+/** Routes for Notes */
+router.post('/:projectId/tasks/:taskId/notes',
+    body('content')
+        .notEmpty().withMessage('El contenido de la nota es obligatorio'),
+        handleInputErrors,
+        NoteController.createNote
+)
+
+
 export default router;
