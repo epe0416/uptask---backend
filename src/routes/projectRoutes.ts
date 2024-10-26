d67a106261ccf729f82825d570a5eaad8828d6a6
Everth Paredes <epe0413@gmail.com>
2024-10-26 15:50:54 -0600

    Creating the Endpoint to delete notes

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -130,5 +130,10 @@ router.post('/:projectId/tasks/:taskId/notes',
 router.get('/:projectId/tasks/:taskId/notes',
     NoteController.getTaskNotes
 )
+router.delete('/:projectId/tasks/:taskId/notes/:noteId',
+    param('noteId').isMongoId().withMessage('ID No Válido'),
+    handleInputErrors,
+    NoteController.deleteNote
+)
 
 export default router;
